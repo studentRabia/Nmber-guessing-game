8be36b6ece8e38bf496d504a770413e7bce053b1
studentRabia <164480298+studentRabia@users.noreply.github.com>
2024-03-25 21:55:41 +0500

    Push my edit work

---
+++ index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+//1) computer wiil generate a rendom number done
+
+//2) user input guessing number done
+
+//3) copmer user input with computer generated number  and show result
+
+
+const randomNumber = Math.floor(Math.random() *6 + 1);
+// console.log(randomNumber );
+
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"Please Guess a Number Between 1-6: ",
+    }
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+console.log("Congtratulation! You Write Right Number.");
+
+}else{
+    console.log("You Guess Rong Number.");
+}
+
